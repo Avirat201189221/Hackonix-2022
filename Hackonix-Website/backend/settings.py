87e03959f45f9e0c0d3bb7f1f7387e658ab852ba
Madhav Goyal <57744109+MadhavGoyal-6@users.noreply.github.com>
2022-08-07 16:18:35 +0530

    add 'hackonix.herokuapp.com' to ALLOWED_HOSTS

--- Hackonix-Website/backend/settings.py
+++ Hackonix-Website/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9xw_1li&ichl-+ajym0--cda!&8f7=)2@)sapqzif_x5uk!-g&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hackonix.herokuapp.com']
 
 
 # Application definition
