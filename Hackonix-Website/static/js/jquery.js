dc545054332ea5c7ff4ffbef3f6de7b001d8b441
Avirat201189221 <avirat0603@gmail.com>
2022-08-25 23:37:46 +0530

    styling modifs & nav click added

--- Hackonix-Website/static/js/jquery.js
+++ Hackonix-Website/static/js/jquery.js
@@ -5,3 +5,25 @@ $(window).scroll(function () {
     $(".navbar").removeClass("fixed-navbar-prop");
   }
 });
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() < 600) {
+    $(".navbar").addClass("fixed-navbar-prop");
+  } else {
+    $(".navbar").removeClass("fixed-navbar-prop");
+  }
+});
+
+if (window.innerWidth < 600) {
+  $(".nav-link-flex").click(() => {
+    $(".navbar-toggler").toggleClass("collapsed");
+    $(".navbar-toggler")[0]["ariaExpanded"] = false;
+    // $(".navbar-collapse").toggleClass("collapsing");
+    // $(".navbar-collapse").toggleClass("collapse");
+    // setTimeout(() => {
+    //   $(".navbar-collapse").toggleClass("collapsing");
+    //   $(".navbar-collapse").toggleClass("collapse");
+    // }, 500);
+    $(".navbar-collapse.collapse").toggleClass("show");
+  });
+}
