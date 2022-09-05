2555c24eaebdb5e2cf853877812539368aaa701e
Avirat201189221 <avirat0603@gmail.com>
2022-09-05 13:33:40 +0530

    set debug to false and add bg to success page

--- Hackonix-Website/backend/settings.py
+++ Hackonix-Website/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9xw_1li&ichl-+ajym0--cda!&8f7=)2@)sapqzif_x5uk!-g&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hackonix.herokuapp.com', '*']
 
